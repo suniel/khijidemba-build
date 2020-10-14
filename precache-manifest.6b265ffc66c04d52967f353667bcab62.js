9a8ac24e7aefebeed77b31a8e88ebf11c5254db1
suniel182 <suniel182@gmail.com>
2020-10-14 16:15:15 +0545

    first commit

---
+++ precache-manifest.6b265ffc66c04d52967f353667bcab62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496cf8a0e23a1f293b8096dab7edd9f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95138056a5775d3de77e",
+    "url": "/static/css/main.a9884042.chunk.css"
+  },
+  {
+    "revision": "b9a94be7bd353bc8829f",
+    "url": "/static/js/2.9c34434b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.9c34434b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95138056a5775d3de77e",
+    "url": "/static/js/main.5d2dc14a.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "28d2bfc0139d4cafa8dfa517ea1aa523",
+    "url": "/static/media/okhaldhunga-district.28d2bfc0.jpg"
+  }
+]);
